0d93070714b261230627c0f73c56b20920476d36
AUTOMATIC1111 <16777216c@gmail.com>
2019-03-19 00:03:13 +0300

    fix chances

--- Source/GradientHairModSettings.cs
+++ Source/GradientHairModSettings.cs
@@ -5,14 +5,14 @@ namespace GradientHair
     public class GradientHairModSettings :ModSettings
     {
         public float chanceMale = 0.02f;
-        public float chanceFemale = 1.0f;
+        public float chanceFemale = 0.25f;
         public bool enable = true;
 
         public override void ExposeData()
         {
             Scribe_Values.Look(ref enable, "enable", true);
             Scribe_Values.Look(ref chanceMale, "chanceMale", 0.02f);
-            Scribe_Values.Look(ref chanceFemale, "chanceFemale", 1.0f);
+            Scribe_Values.Look(ref chanceFemale, "chanceFemale", 0.25f);
         }
     }
 }
