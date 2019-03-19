b3453612911cea101cca1e36ed907476ed4e6dd6
AUTOMATIC1111 <16777216c@gmail.com>
2019-03-19 20:56:39 +0300

    added public api

---
+++ Source/PublicApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace GradientHair
+{
+    public class PublicApi
+    {
+        public static void SetPawnGradientHair(Pawn pawn, bool enabled, Color color)
+        {
+            CompGradientHair comp = pawn.GetComp<CompGradientHair>();
+            if (comp == null) return;
+
+            GradientHairSettings settings = comp.Settings;
+
+            settings.enabled = enabled;
+            settings.colorB = color;
+        }
+    }
+}
