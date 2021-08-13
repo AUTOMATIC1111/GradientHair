c44bb0f494d297c5ff9c6c72f0d28c4128e1008b
AUTOMATIC <16777216c@gmail.com>
2021-08-13 09:19:28 +0300

    HAR support and a workaround around HAR bug

--- Source/Patch/PawnGraphicSet.cs
+++ Source/Patch/PawnGraphicSet.cs
@@ -9,6 +9,7 @@ using Verse;
 
 namespace GradientHair.Patch
 {
+    // this actually changes the hair graphic to a gradient-enabled one
     [HarmonyPatch(typeof(PawnGraphicSet), "CalculateHairMats")]
     class PawnGraphicSetCalculateHairMats
     {
@@ -20,6 +21,23 @@ namespace GradientHair.Patch
             GradientHairSettings settings = comp.Settings;
             if (!settings.enabled) return;
 
+            __instance.hairGraphic = Graphic_MultiMask.Get(__instance.pawn.story.hairDef.texPath, settings.mask, __instance.pawn.story.hairColor, settings.colorB);
+        }
+    }
+
+    // we do it again in case another mod overweites it
+    [HarmonyPatch(typeof(PawnGraphicSet), "ResolveApparelGraphics")]
+    class PawnGraphicSetResolveApparelGraphics
+    {
+        static void Postfix(PawnGraphicSet __instance)
+        {
+            CompGradientHair comp = __instance.pawn.GetComp<CompGradientHair>();
+            if (comp == null) return;
+
+            GradientHairSettings settings = comp.Settings;
+            if (!settings.enabled) return;
+
+            if (__instance.hairGraphic is Graphic_MultiMask) return;
             __instance.hairGraphic = Graphic_MultiMask.Get(__instance.pawn.story.hairDef.texPath, settings.mask, __instance.pawn.story.hairColor, settings.colorB);
         }
     }
--- Source/StylingStation/PatchPawnStyleItemChooser.cs
+++ Source/StylingStation/PatchPawnStyleItemChooser.cs
@@ -12,7 +12,7 @@ namespace GradientHair.StylingStation
 
     // the result of this function is used for ordering in the dialog
     [HarmonyPatch(typeof(PawnStyleItemChooser), "StyleItemChoiceLikelihoodFor")]
-    class PatchPawnStyleItemChooser
+    class PatchPawnStyleItemChooserStyleItemChoiceLikelihoodFor
     {
         static bool Prefix(ref float __result, StyleItemDef styleItem)
         {
@@ -21,6 +21,23 @@ namespace GradientHair.StylingStation
             __result = -styleItem.index;
 
             return false;
+        }
+    }
+
+    // prevent the bug in HAR related to missing key dict in https://github.com/erdelf/AlienRaces/blob/c854ad7845801ac9d98bcf6b09e9af7ec9e36742/Source/AlienRace/AlienRace/HarmonyPatches.cs#L525
+    [HarmonyPatch(typeof(PawnStyleItemChooser), "WantsToUseStyle")]
+    class PatchPawnStyleItemChooserWantsToUseStyle
+    {
+        [HarmonyBefore(new string[] { "rimworld.erdelf.alien_race.main" })]
+        static bool Prefix(ref bool __result, Pawn pawn, StyleItemDef styleItemDef)
+        {
+            if (styleItemDef is GradientHairMaskDef)
+            {
+                __result = false;
+                return false;
+            }
+
+            return true;
         }
     }
 }
